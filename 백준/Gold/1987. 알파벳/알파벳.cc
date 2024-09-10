6d953eb622a15e74b9540f56eb615034e985a5c7
jki12 <sjak0227@naver.com>
2024-09-10 15:14:16 +0900

    [Gold IV] Title: 알파벳, Time: 340 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,75 +1,59 @@
-    // #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
-    #include <iostream>
-    #include <algorithm>
+using namespace std;
 
-    using namespace std;
+#define SIZE 32
 
-    #define SIZE 20
+int r, c;
+int ans = 1;
 
-    int r, c;
-    int ans = 1;
+char board[SIZE][SIZE];
 
-    char board[SIZE][SIZE];
+int dx[] = { 0, 0, 1, -1 };
+int dy[] = { 1, -1, 0, 0 };
 
-    int dx[] = { 0, 0, 1, -1 };
-    int dy[] = { 1, -1, 0, 0 };
+int get_count(int mask) {
+    int count = 0;
 
-    int get_count(int mask) {
-        int count = 0;
-
-        while (mask) {
-            if (mask & 1) count++;
-            mask >>= 1;
-        }
-
-        return count;
+    while (mask) {
+        if (mask & 1) count++;
+        mask >>= 1;
     }
 
-    void recur(int x, int y, int mask, int cnt) {
-        int flag = 1 << (board[x][y] - 65);
+    return count;
+}
 
-        // cout << "x : " << x << ' ' << "y : " << y << '\n';
+void recur(int x, int y, int mask, int cnt) {
+    int flag = 1 << (board[x][y] - 65);
+    // ans = max(ans, cnt);
 
-        // if (mask & flag) {
-        //     ans = max(ans, get_count(mask));
-        //     // cout << "ans : " << ans << '\n';
+    mask |= flag;
+    
+    for (int i = 0; i < 4; ++i) {
+        int nx = x + dx[i];
+        int ny = y + dy[i];
 
-        //     return;
-        // }
-
-        ans = max(ans, cnt);
-        
-        mask |= flag;
-
-        for (int i = 0; i < 4; ++i) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-
-            if (0 > nx || nx >= r || 0 > ny || ny >= c || mask & (1 << (board[nx][ny] - 65))) {
-                // ans = max(ans, get_count(mask));
-                continue;
-            }
-            // if (mask & (1 << (board[nx][ny] - 65))) continue;
-
-            recur(nx, ny, mask, cnt + 1);
-        }
-        
-        // cout << '\n';
-    }
-
-    int main(void) {
-        cin >> r >> c;
-
-        string s;
-        for (int i = 0; i < r; ++i) {
-            cin >> s;
-            for (int j = 0; j < c; ++j) {
-                board[i][j] = s[j];
-            }
+        if (0 > nx || nx >= r || 0 > ny || ny >= c || mask & (1 << (board[nx][ny] - 65))) {
+            ans = max(ans, cnt);
+            continue;
         }
 
-        recur(0, 0, 0, 1);
-
-        cout << ans << '\n';
+        recur(nx, ny, mask, cnt + 1);
     }
+}
+
+int main(void) {
+    cin >> r >> c;
+
+    string s;
+    for (int i = 0; i < r; ++i) {
+        cin >> s;
+        for (int j = 0; j < c; ++j) {
+            board[i][j] = s[j];
+        }
+    }
+
+    recur(0, 0, 0, 1);
+
+    cout << ans << '\n';
+}
