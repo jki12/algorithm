c74ac80f52f292b03ff67a1296c6b1bd3619400a
jki12 <sjak0227@naver.com>
2024-09-11 12:25:42 +0900

    Update 경사로.cc

--- 백준/Gold/14890. 경사로/경사로.cc
+++ 백준/Gold/14890. 경사로/경사로.cc
@@ -2,10 +2,9 @@
 
 using namespace std;
 
-#define SIZE 128
+#define SIZE 256
 
 int n, l, ans;
-
 int board[SIZE][SIZE];
 
 int main(void) {
@@ -15,7 +14,11 @@ int main(void) {
         cin >> board[i / n][i % n];
     }
 
-    for (int i = 0; i < n; ++i) { // col.
+    for (int i = 0; i < n * n; ++i) {
+        board[n + i / n][i % n] = board[i % n][i / n];
+    }
+
+    for (int i = 0; i < n * 2; ++i) { // col.
         int h = board[i][0];
 
         int mark[SIZE] = { 0, };
@@ -48,42 +51,6 @@ int main(void) {
         }
         ans++;
     next:
-        continue;
-    }
-
-    for (int i = 0; i < n; ++i) { // row.
-        int h = board[0][i];
-
-        int mark[SIZE] = { 0, };
-        for (int j = 1; j < n; ++j) {
-
-            if (abs(board[j][i] - h) > 1) goto next2;
-            if (board[j][i] == h) continue;
-
-            int temp = l;
-            if (h < board[j][i]) { // 높아진 경우.
-                if (j - l < 0) goto next2;
-
-                while (temp) {
-                    if (h != board[j - temp][i] || mark[j - temp]) goto next2;
-
-                    mark[j - temp] = true;
-                    temp--;
-                }
-            }
-            else if (h > board[j][i]) { // 낮아진 경우.
-                if (j + l > n) goto next2;
-
-                while (temp--) {
-                    if (board[j][i] != board[j + temp][i] || mark[j + temp]) goto next2;
-                    mark[j + temp] = true;
-                }
-            }
-
-            h = board[j][i];
-        }
-        ans++;
-    next2:
         continue;
     }
 
