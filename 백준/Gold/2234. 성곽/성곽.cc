b9168ed563f2034ca22ae21e17cac794657c44af
jki12 <sjak0227@naver.com>
2024-09-12 12:51:28 +0900

    [Gold III] Title: 성곽, Time: 0 ms, Memory: 2072 KB -BaekjoonHub

---
+++ 백준/Gold/2234. 성곽/성곽.cc
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define SIZE (64)
+
+int n, m, x, y, cnt, _max, _max2;
+int board[SIZE][SIZE];
+int vis[SIZE][SIZE];
+int arr[SIZE * SIZE];
+
+int dx[] = { 0, -1, 0, 1 }; // wnes.
+int dy[] = { -1, 0, 1, 0 };
+
+void recur(pair<int, int> s) {
+    if (vis[s.first][s.second]) return;
+
+    cnt++;
+    queue<pair<int, int>> q;
+    q.push(s);
+    vis[s.first][s.second] = cnt;
+
+    int temp = 0;
+    while (!q.empty()) {
+        tie(x, y) = q.front(); q.pop();
+        // cout << x << ' ' << y << '\n';
+        temp++;
+
+        for (int i = 0; i < 4; ++i) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (0 > nx || nx >= n || 0 > ny || ny >= m || board[x][y] & (1 << i)) continue;
+            if (vis[nx][ny]) continue;
+
+            q.push({nx, ny});
+            vis[nx][ny] = cnt;
+        }
+    }
+
+    arr[cnt] = temp;
+    // cout << "cnt : " << cnt << ' ' << "size : " << temp << '\n';
+    _max = max(_max, temp);
+}
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    cin >> m >> n;
+
+    for (int i = 0; i < n * m; ++i) {
+        cin >> board[i / m][i % m];
+    }
+
+    for (int i = 0; i < n * m; ++i) {
+        recur({i / m, i % m});
+    }
+
+    for (int i = 0; i < n * m; ++i) {
+
+        for (int j = 0; j < 4; ++j) {
+            int nx = i / m + dx[j];
+            int ny = i % m + dy[j];
+
+            if (0 > nx || nx >= n || 0 > ny || ny >= m) continue;
+
+            if (vis[i / m][i % m] != vis[nx][ny]) {
+                _max2 = max(_max2, arr[vis[i / m][i % m]] + arr[vis[nx][ny]]);
+            }
+
+        }
+    }
+
+    // cout << board[3][0] << ' ' << board[3][1] << '\n';
+    // cout << vis[3][0] << ' ' << vis[3][1] << '\n';
+    // cout << arr[vis[3][0]] << ' ' << arr[vis[3][1]] << '\n';
+
+    cout << cnt << ' ' << _max << ' ' << _max2 << '\n'; // exp : 5 9 16
+}
