e476bbbf60d6d65a1536f4ec41e13a7505ff6621
jki12 <sjak0227@naver.com>
2024-10-08 23:59:18 +0900

    [Silver V] Title: 단어 정렬, Time: 76 ms, Memory: 6728 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n;
+
+bool compare(string a, string b) {
+    if (a.size() == b.size()) return (a.compare(b) < 0);
+
+    return a.size() < b.size();
+}
+
+int main(void) {
+    cin >> n;
+    vector<string> v(n);
+    vector<string> unique;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> v[i];
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int i = 0; i < n; ++i)
+        if (i == 0 || v[i] != v[i - 1]) unique.push_back(v[i]);
+
+    sort(unique.begin(), unique.end(), compare);
+
+    for (int i = 0; i < unique.size(); ++i) cout << unique[i] << '\n';
+}
